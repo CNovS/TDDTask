7116262a7b7fcbb69655b552133186ad8bc95a60
Carla <cnovella94@gmail.com>
2025-06-26 13:08:13 +0200

    feat: add more test cases

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,13 +1,19 @@
 import { helloWorld } from './index';
 
 describe('TDD task', () => {
+    //Start
     it.todo('should start in the scoreboard with 0 - 0 as score');
-   it.todo('should update the score to the one passed by');
-   it.todo('should remove the finished match from the scoreboard');
-    //TODO: Describe summary
-   it.todo('should return the summary with the finished matches ordered by their total score');
-   it.todo('should return all matches with the same total score, ordered by the most recently started one');
-   it.todo('should return empty summary if no matches are started');
-   it.todo('should return empty summary if no matches are finished');
-    //TODO: Finish Describe summary
+    it.todo('should return an empty scoreboard if no matches are started');
+    it.todo('should return an empty scoreboard if any team has empty value when starting a match');
+    //Update
+    it.todo('should update the score of the match with the given score values');
+    it.todo('should not update the score of the match if no valid scores are passed');
+    //Finish
+    it.todo('should remove the finished match from the scoreboard');
+    //Summary
+    it.todo('should return the summary with the finished matches ordered by their total score');
+    it.todo('should return all matches with the same total score, ordered by the most recently started one');
+    it.todo('should return empty summary if no matches are started');
+    it.todo('should return empty summary if no matches are finished');
+    it.todo('should return empty summary if the match to finish does not exist');
 });
